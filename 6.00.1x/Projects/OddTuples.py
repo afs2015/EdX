0f00c1c7b002fb9fd20ad3e9910343694eefef96
Andrew Selzer <andrewfselzer@gmail.com>
2013-11-06 17:48:02 -0500

    Adding OddTuples.py
    Finds every odd tuple, from lecture 6

---
+++ 6.00.1x/Projects/OddTuples.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Author: Andrew Selzer
+
+def oddTuples(aTup):
+    '''
+    aTup: a tuple
+    
+    returns: tuple, every other element of aTup. 
+    '''
+    # Your Code Here
+    return aTup[::2]
