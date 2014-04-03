fef545c3c1c59d744e2812644a82f02d3a3a8fbc
Andrew Selzer <andrewfselzer@gmail.com>
2014-04-03 12:24:34 -0400

    Uploading 6.00.2x Files
    Adding Lecture 1 files.

---
+++ 6.00.2x/Lecture Code/tempsLoad.py
@@ -0,0 +1,26 @@
+import pylab
+import numpy as np
+
+def loadFile():
+    inFile = open('C:/Users/Apache/Documents/GitHub/EdX/6.00.2x/Lecture Code/julyTemps.txt')
+    high = []
+    low = []
+    for line in inFile:
+        fields = line.split()
+        if len(fields) != 3 or 'Boston' == fields[0] or 'Day' == fields[0]:
+            continue
+        else:
+            high.append(int(fields[1]))
+            low.append(int(fields[2]))
+    return (low, high)
+    
+def producePlot(lowTemps, highTemps):
+    diffTemps = list(np.array(highTemps) - np.array(lowTemps))
+    pylab.plot(range(1,32), diffTemps)
+    pylab.title('Day by Day Ranges in Temperature in Boston in July 2012')
+    pylab.xlabel('Days')
+    pylab.ylabel('Temperature Ranges')
+    pylab.show()
+    
+(low, high) = loadFile()    
+producePlot(low, high)
